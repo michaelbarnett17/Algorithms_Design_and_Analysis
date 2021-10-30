032160e12b1557c80be7304cc3bfa129b7f46246
michaelbarnett17 <michaelbarnett17@gmail.com>
2021-10-30 11:46:52 -0500

    Created files for Homework 5

---
+++ hw5/Graph.java
@@ -0,0 +1,12 @@
+package hw5;
+
+import java.io.*;
+import java.util.*;
+
+class Graph
+{
+    public Graph(String filename) 
+    {
+        System.out.println("I'm your graph for homework 5!");
+    }
+}
---
+++ hw5/Homework5.java
@@ -0,0 +1,14 @@
+package hw5;
+
+import java.util.*;
+
+class Homework5 
+{
+    public static void main(String args[])
+    {
+        String fileName = "hw5\\dijkstraData_test.txt";
+        // String fileName = String fileName = "hw5\\dijkstraData.txt";
+
+        Graph graph = new Graph(fileName);
+    }
+}
